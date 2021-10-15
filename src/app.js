45bbdc5cf5ca5662203b08ab0dbec160266291fb
ElFecho0 <fflores.monsivais@gmail.com>
2021-10-15 00:34:46 +0000

    Tarea Hecha

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for(let i = 0; i < pronoun.length; i++) {
+  for (let x = 0; x < adj.length; x++) {
+    for (let y = 0; y < noun.length; y++) {
+      console.log(pronoun[i]+adj[x]+noun[y]+".com")
+    }
+  }
+}
